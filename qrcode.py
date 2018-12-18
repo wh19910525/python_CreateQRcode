dcb7f9feb8ce83f40ace330b85d94e4df2806cbc
qinchengqiang <qinchengqiang@xgd.com>
2018-12-18 14:09:16 +0800

    first init

---
+++ qrcode.py
@@ -0,0 +1,13 @@
+# qrcode.py
+#coding:utf-8
+
+#from PIL import Image, ImageDraw
+
+aa = 'abbdbdbbf'
+bb = 'a海'
+print len(aa)
+print len(bb)
+
+print "----------- %r -----------" % aa.count('b')
+
+
