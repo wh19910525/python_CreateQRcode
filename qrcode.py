b3f79d33533298919233c34a62e7d4b2c6acb43c
qinchengqiang <qinchengqiang@xgd.com>
2018-12-18 14:21:43 +0800

    1. add _genImage 2. check _genImage

--- qrcode.py
+++ qrcode.py
@@ -2,12 +2,53 @@
 #coding:utf-8
 
 from PIL import Image, ImageDraw
+from numpy import *;
 
 aa = 'abbdbdbbf'
-bb = 'a海'
-print len(aa)
-print len(bb)
 
-print "----------- %r -----------" % aa.count('b')
+print "----------- start, %r -----------" % aa.count('b')
 
+'''
+参数:
+    bitmap        :QR码矩阵
+    qrcodesize    :图像宽度, 单位:像素
+    filename      :保存文件名
+'''
+def _genImage(bitmap, qrcodesize, filename):
+    width = qrcodesize
+    height = qrcodesize
+    '''
+    Generate image corresponding to the input bitmap
+    with specified qrcodesize and filename.
+    '''
+    # New image in black-white mode initialized with white.
+    img = Image.new('1', (width, height), 'white')
+    drw = ImageDraw.Draw(img)
+
+    # Normalized pixel width.
+    print "bitmap-len=%r" % (len(bitmap))
+    print "bitmap-len=%r" % (bitmap)
+
+    a_unit_size = qrcodesize / len(bitmap)
+    print "a-rectangle-size=%r" % (a_unit_size)
+
+    for y in range(width):
+        # Normalized y coordinate in bitmap
+        normalj = y / a_unit_size
+
+        for x in range(height):
+            # Normalized x coordinate in bitmap
+            normali = x / a_unit_size
+
+            if normalj < len(bitmap) and normali < len(bitmap):
+                # Draw pixel.
+                drw.point((x, y), fill=bitmap[normalj][normali])
+
+    img.save(filename)
+
+print "------------ end, %r ------------" % aa.count('b')
+
+test = [[ (i+j)%2 for i in range(8) ] for j in range(8)]
+
+_genImage(test, 240, 'test.png')
 
